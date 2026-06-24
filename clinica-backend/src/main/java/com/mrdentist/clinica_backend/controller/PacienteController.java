c7fa2c5dbc04b116723a776f10e96114c4473ddb
Baruch44498 <baruchanticona456@gmail.com>
2026-06-23 22:18:37 -0500

    feat(backend): implementacion del crud de pacientes con conexion a mysql (#1)

---
+++ clinica-backend/src/main/java/com/mrdentist/clinica_backend/controller/PacienteController.java
@@ -0,0 +1,36 @@
+package com.mrdentist.clinica_backend.controller;
+import com.mrdentist.clinica_backend.entity.Paciente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.mrdentist.clinica_backend.service.PacienteService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pacientes")
+@CrossOrigin(origins = "http://localhost:4200") // Permite que Angular (Frontend) acceda a estos datos
+public class PacienteController
+{
+    @Autowired
+    private PacienteService pacienteService;
+
+    @GetMapping
+    public List<Paciente> listarPacientes() {
+        return pacienteService.listarActivos();
+    }
+
+    @PostMapping
+    public Paciente registrarPaciente(@RequestBody Paciente paciente) {
+        return pacienteService.guardar(paciente);
+    }
+
+    @PutMapping("/{id}")
+    public Paciente actualizarPaciente(@PathVariable Long id, @RequestBody Paciente paciente) {
+        return pacienteService.actualizar(id, paciente);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminarPaciente(@PathVariable Long id) {
+        pacienteService.eliminarLogico(id);
+    }
+}
---
+++ clinica-backend/src/main/java/com/mrdentist/clinica_backend/entity/Paciente.java
@@ -0,0 +1,117 @@
+package com.mrdentist.clinica_backend.entity;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "pacientes")
+public class Paciente
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPaciente;
+
+    @Column(nullable = false, length = 100)
+    private String nombres;
+
+    @Column(nullable = false, length = 100)
+    private String apellidos;
+
+    @Column(nullable = false, unique = true, length = 8)
+    private String dni;
+
+    @Column(length = 15)
+    private String telefono;
+
+    @Column(unique = true, length = 100)
+    private String correo;
+
+    @Column(name = "fecha_registro")
+    private LocalDate fechaRegistro;
+
+    // Cumple con RNF11: Eliminación lógica
+    @Column(nullable = false)
+    private Boolean estado = true;
+
+    @PrePersist
+    public void prePersist() {
+        this.fechaRegistro = LocalDate.now(); // Asigna la fecha actual automáticamente
+    }
+
+    public Paciente(Long idPaciente, String nombres, String apellidos, String dni, String telefono, String correo, LocalDate fechaRegistro, Boolean estado) {
+        this.idPaciente = idPaciente;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.dni = dni;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.fechaRegistro = fechaRegistro;
+        this.estado = estado;
+    }
+
+    public Long getIdPaciente() {
+        return idPaciente;
+    }
+
+    public void setIdPaciente(Long idPaciente) {
+        this.idPaciente = idPaciente;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    public Paciente() {
+    }
+}
