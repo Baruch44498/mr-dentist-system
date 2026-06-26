09ef3081227b8364563de721549d50652fc2c93e
Baruch44498 <baruchanticona456@gmail.com>
2026-06-25 22:06:20 -0500

    feat(frontend): agregar crud de especialidades odontologicas (#10)
    Co-authored-by: Alex17redLion <alexander.pinillosperez@gmail.com>

--- clinica-frontend/src/app/app.routes.ts
+++ clinica-frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { DashboardComponent } from './components/dashboard/dashboard';
 import { PacienteListComponent } from './components/paciente-list/paciente-list';
 import { MedicoListComponent } from './components/medico-list/medico-list';
 import { CitaListComponent } from './components/cita-list/cita-list';
+import { EspecialidadListComponent } from './components/especialidad-list/especialidad-list';
 
 export const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'pacientes', component: PacienteListComponent },
   { path: 'medicos', component: MedicoListComponent },
+  { path: 'especialidades', component: EspecialidadListComponent },
   { path: 'citas', component: CitaListComponent },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: '**', redirectTo: 'dashboard' }
